6b078623ef95be28764b3f47c299903955844b1c
Elad Hirsch <eladh@jfrog.com>
2019-01-24 19:45:51 +0200

    default container app

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -40,7 +40,7 @@ podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
 
                 configFileProvider(
                         [configFile(fileId: 'private_key' ,variable: 'KEY')]) {
-                    sh 'echo "${env.KEY}" >> /etc/docker/certs.d/test-docker-reg\\:5000/artifactory.crt'
+                    sh 'printf "${env.KEY}" '
                 }
 
 //                sh("cp ca.crt /etc/docker/certs.d/test-docker-reg\\:5000/")
