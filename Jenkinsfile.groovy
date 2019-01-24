a846128c05f9257861daa682d6079757666aa2f6
Elad Hirsch <eladh@jfrog.com>
2019-01-24 19:27:45 +0200

    default container app

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -41,7 +41,7 @@ podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
                 configFileProvider(
                         [configFile(fileId: 'private_key',targetLocation : '/tmp/artifactory.crt' ,variable: 'KEY')]) {
                     echo " =========== ^^^^^^^^^^^^ Reading config from pipeline script "
-                    sh 'cp /tmp/artifactory.crt >> /etc/docker/certs.d/test-docker-reg\\:5000'
+                    sh 'cp /tmp/artifactory.crt >> /etc/docker/certs.d/test-docker-reg\\:5000/'
                     echo " =========== ~~~~~~~~~~~~ ============ "
                 }
 
