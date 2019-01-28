40246d6b03c88e2551344f40ef77c5c15c968d0d
elad hirsch <hirsch.elad@gmail.com>
2019-01-28 14:09:44 +0200

    add

--- Jenkinsfile-dist.groovy
+++ Jenkinsfile-dist.groovy
@@ -1,15 +1,15 @@
-server = Artifactory.server "artifactory-server"
+server = Artifactory.server "artifactory"
 rtFullUrl = server.url
+rtIpAddress = rtFullUrl - ~/^http?.:\/\// - ~/\/artifactory$/
+
+buildInfo = Artifactory.newBuildInfo()
 
 
 podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
-        containerTemplate(name: 'helm', image: 'lachlanevenson/k8s-helm:v2.6.0', command: 'cat', ttyEnabled: true),
-        containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:latest', command: 'cat', ttyEnabled: true)
-],
-        volumes:[
-                hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
-        ]) {
-
+        containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true , privileged: true),
+        containerTemplate(name: 'node', image: 'node:8', command: 'cat', ttyEnabled: true)
+] ,volumes: [
+        hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock')]) {
 
     node('jenkins-pipeline') {
 
@@ -18,21 +18,107 @@ podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
         }
 
         stage('Clone sources') {
-            git url: 'http://35.202.14.162/elad/app-repo.git'
+            git url: 'https://github.com/eladh/docker-app-demo.git', credentialsId: 'github'
         }
 
-        stage('Run kubectl') {
-            container('kubectl') {
-                sh "kubectl get pods"
+        stage('Download Dependencies') {
+            try {
+                def pipelineUtils = load 'pipelineUtils.groovy'
+                pipelineUtils.downloadArtifact(rtFullUrl, "gradle-local", "*demo-gradle/*", "jar", buildInfo, false)
+                pipelineUtils.downloadArtifact(rtFullUrl, "npm-local", "*client-app*", "tgz", buildInfo, true)
+            } catch (Exception e) {
+                println "Caught Exception during resolution. Message ${e.message}"
+                throw e as java.lang.Throwable
             }
         }
 
-        stage('Helm install') {
-            container('helm') {
-                sh 'helm ls'
+        stage('Docker build') {
+            def rtDocker = Artifactory.docker server: server
+
+            container('docker') {
+                docker.withRegistry("https://docker.artifactory.jfrog.com", 'artifactorypass') {
+                    groovy.lang.GString dockerImageTag = "docker.artifactory.jfrog.com/docker-app:${env.BUILD_NUMBER}"
+                    def dockerImageTagLatest = "docker.artifactory.jfrog.com/docker-app:latest"
+
+                    buildInfo.env.capture = true
+
+                    docker.build(dockerImageTag)
+                    docker.build(dockerImageTagLatest)
+
+
+                    rtDocker.push(dockerImageTag, "docker-local", buildInfo)
+                    rtDocker.push(dockerImageTagLatest, "docker-local", buildInfo)
+                    server.publishBuildInfo buildInfo
+                }
             }
+        }
 
+        stage('Docker Integration Tests') {
 
+            container('docker') {
+                docker.withRegistry("https://docker.artifactory.jfrog.com", 'artifactorypass') {
+                    groovy.lang.GString tag = "docker.artifactory.jfrog.com/docker-app:${env.BUILD_NUMBER}"
+                    docker.image(tag).withRun('-p 9191:81 -e “SPRING_PROFILES_ACTIVE=local” ') { c ->
+                        sleep 10
+                        def stdout = sh(script: 'wget "http://localhost:9191/index.html"', returnStdout: true)
+                        println stdout
+//            if (stdout.contains("client-app")) {
+//                println "*** Passed Test: " + stdout
+//                println "*** Passed Test"
+//                return true
+//            } else {
+//                println "*** Failed Test: " + stdout
+//                return false
+//            }
+                    }
+                }
+            }
         }
     }
 }
+
+
+
+
+//    stage('Helm install') {
+//        docker.image('docker.bintray.io/jfrog/jfrog-cli-go:latest').inside {
+//            sh "ls"
+//        }
+//
+//    }
+//
+//    //Scan Build Artifacts in Xray
+//    stage('Xray Scan') {
+//        if (XRAY_SCAN == "YES") {
+//            java.util.LinkedHashMap<java.lang.String, java.lang.Boolean> xrayConfig = [
+//                    'buildName' : env.JOB_NAME,
+//                    'buildNumber' : env.BUILD_NUMBER,
+//                    'failBuild' : false
+//            ]
+//            def xrayResults = server.xrayScan xrayConfig
+//
+//            if (xrayResults.isFoundVulnerable()) {
+//                error('Stopping early… got Xray issues ')
+//            }
+//        } else {
+//            println "No Xray scan performed. To enable set XRAY_SCAN = YES"
+//        }
+//    }
+//
+//    stage('Promote Docker image') {
+//        java.util.LinkedHashMap<java.lang.String, java.lang.Object> promotionConfig = [
+//                'buildName'  : buildInfo.name,
+//                'buildNumber': buildInfo.number,
+//                'targetRepo' : "docker-prod-local",
+//                'comment'    : 'This is a stable docker image',
+//                'status'     : 'Released',
+//                'sourceRepo' : 'docker-stage-local',
+//                'copy'       : true,
+//                'failFast'   : true
+//        ]
+//
+//        server.promote promotionConfig
+//    }
+
+
+//    }
