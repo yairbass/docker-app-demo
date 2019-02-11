dff36add80b845d7d25437f92a2faafc94291897
elad hirsch <hirsch.elad@gmail.com>
2019-02-10 21:44:58 -0800

    add

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -95,37 +95,6 @@ podTemplate(label: 'dind-template' , cloud: 'k8s' , containers: [
     }
 }
 
-podTemplate(label: 'helm-template' , cloud: 'k8s' , containers: [
-        containerTemplate(name: 'jfrog-cli', image: 'docker.bintray.io/jfrog/jfrog-cli-go:latest', command: 'cat', ttyEnabled: true) ,
-        containerTemplate(name: 'helm', image: 'alpine/helm:latest', command: 'cat', ttyEnabled: true) ]) {
-
-    node('helm-template') {
-        stage('Build Chart & push it to Artifactory') {
-
-            git url: 'https://github.com/eladh/docker-app-demo.git', credentialsId: 'github'
-            def pipelineUtils = load 'pipelineUtils.groovy'
-
-            def aqlString = 'items.find ({"repo":"docker-local","type":"folder","$and":' +
-                    '[{"path":{"$match":"docker-app*"}},{"path":{"$nmatch":"docker-app/latest"}}]' +
-                    '}).include("path","created","name").sort({"$desc" : ["created"]}).limit(1)'
-
-
-            def artifactInfo = pipelineUtils.executeAql(rtFullUrl, aqlString)
-            def dockerTag = artifactInfo ? artifactInfo.name : "latest"
-
-            container('helm') {
-                sh "helm init --client-only"
-                sh "sed -i 's/latest/${dockerTag}/g' helm-chart-docker-app/values.yaml"
-                sh "helm package helm-chart-docker-app"
-            }
-            container('jfrog-cli') {
-                pipelineUtils.pushHelmChart("helm-local" ,"helm-chart-docker-app" ,dockerTag)
-            }
-        }
-    }
-}
-
-
 podTemplate(label: 'promote-template' , cloud: 'k8s' , containers: []) {
 
     node('promote-template') {
@@ -153,7 +122,7 @@ podTemplate(label: 'promote-template' , cloud: 'k8s' , containers: []) {
                     'targetRepo' : "docker-prod-local",
                     'comment'    : 'This is a stable docker image',
                     'status'     : 'Released',
-                    'sourceRepo' : 'docker-stage-local',
+                    'sourceRepo' : 'docker-local',
                     'copy'       : true,
                     'failFast'   : true
             ]
