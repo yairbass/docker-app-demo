e1a1575432925b3f2ac5cf0ae12ed5178069266a
elad hirsch <hirsch.elad@gmail.com>
2019-02-13 14:52:26 -0800

    trusted cert script

--- add-trusted-cert.sh
+++ add-trusted-cert.sh
@@ -1,6 +1,6 @@
     CERT=`cat docker.crt`
 
-    kubectl get nodes |cut -f1 -d\  | while read node;do
+    kubectl get no -o name | while read node;do
     echo $node
     gcloud compute ssh --zone "$1" $node --command \
             " sudo su -c ' mkdir -p /etc/docker/certs.d/docker.artifactory.$2.jfrog.com' root &&
