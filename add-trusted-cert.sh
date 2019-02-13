e4c5b213ac98f7e0353153e352e5c0e25b5c7222
elad hirsch <hirsch.elad@gmail.com>
2019-02-13 14:37:45 -0800

    trusted cert script

--- add-trusted-cert.sh
+++ add-trusted-cert.sh
@@ -1,9 +1,12 @@
 CERT=`cat docker.crt`
+    echo $1
+    echo $2
+    echo $CERT
 
-kubectl get node | cut -f1 -d\  | while read node;do
-    echo $node
-    gcloud compute ssh --zone "$1" $node --command \
-            " sudo su -c ' mkdir -p /etc/docker/certs.d/docker.artifactory.$2.jfrog.com' root &&
-    echo '$CERT' > /tmp/ca.crt &&
-            sudo su -c ' mv /tmp/ca.crt /etc/docker/certs.d/docker.artifactory.$2.jfrog.com/' root"  </dev/null;
-done
+#kubectl get node | cut -f1 -d\  | while read node;do
+#    echo $node
+#    gcloud compute ssh --zone "$1" $node --command \
+#            " sudo su -c ' mkdir -p /etc/docker/certs.d/docker.artifactory.$2.jfrog.com' root &&
+#    echo '$CERT' > /tmp/ca.crt &&
+#            sudo su -c ' mv /tmp/ca.crt /etc/docker/certs.d/docker.artifactory.$2.jfrog.com/' root"  </dev/null;
+#done
